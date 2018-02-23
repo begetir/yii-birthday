d0b2ed6462245def83915adbcfc1d414a7da208e
Beget <tameshki.ir@gmail.com>
2018-02-23 12:32:45 +0330

    edit

--- Birthday.php
+++ Birthday.php
@@ -5,7 +5,7 @@ namespace beget\birthday;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class Birthday extends \yii\base\Widget
 {
     public function run()
     {
